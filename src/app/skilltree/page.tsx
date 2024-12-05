86ed31567dd5c7ac4dcde9ac9ae1f1f84842cf48
buzzdead <sigmundvolden@gmail.com>
2024-12-05 14:57:15 +0100

    mobile

--- src/app/skilltree/page.tsx
+++ src/app/skilltree/page.tsx
@@ -1,9 +1,33 @@
-"use client"
+"use client";
 
+import { useEffect, useState } from "react";
 import SkillTreeMain from "./SkillTreeMain";
 
 const SkillTree = () => {
-    return <SkillTreeMain />
-}
+  const [isMobileOrPad, setIsMobileOrPad] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileOrPad(window.innerWidth < 1000); // Check if width is less than 1000px
+    };
+
+    // Initial check
+    handleResize();
+
+    // Add event listener for resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup on unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div className={isMobileOrPad ? "w-[110rem] h-[110rem]" : ""}>
+      <SkillTreeMain />
+    </div>
+  );
+};
 
 export default SkillTree;
